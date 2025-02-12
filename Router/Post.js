f9a929eeed91d755f48cad7b7c2865caacca508c
rajendra-chimala <rajendrachimala5@gmail.com>
2025-02-12 16:34:32 +0545

    change

--- Router/Post.js
+++ Router/Post.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const verifyToken = require('../Middlewares/authUSer');
 const {createPost, deletePost, getAllPosts, editPost, getPostById} = require('../controller/Post');
+const Post = require('../model/Post');
 
 
 const router = express.Router();
@@ -10,6 +11,12 @@ router.delete  ('/delete/:id',verifyToken,deletePost);
 router.put('/edit/:id',verifyToken, editPost);
 router.get('/posts',verifyToken, getAllPosts);
 router.get('/post/:id',verifyToken, getPostById);
+
+router.get('/posts-admin',async (req,res)=>{
+    const all = await Post.find();
+
+    res.json(all);
+})
 
 
 
